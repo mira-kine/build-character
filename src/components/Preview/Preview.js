343c3ed7bbbf58c5714045f2cd32efedff799d3b
mira-kine <mirak0419@gmail.com>
2021-12-01 16:53:05 -0800

    connecting images to corresponding values

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-export default function Preview() {
-  return <div></div>;
+export default function Preview({ head, middle, bottom }) {
+  return (
+    <div className="character">
+      <img alt={head} src={`${process.env.PUBLIC_URL}/assets/${head}.png`}></img>
+    </div>
+  );
 }
